74789ed94b21cc75f6022ef82327b3fdd2d488e5
Adrian <adriantutuianu01@gmail.com>
2022-06-20 23:18:29 +0100

    Added header text

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,13 @@
 import "./header.css";
+
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Phonebook 2022</h1>
+    </header>
+  );
+};
+
+export default Header;
